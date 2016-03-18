cf04bca8c3a35ee706646bf93830d9d9f5c29bf2
CellBody <austintingwork@gmail.com>
2016-03-18 13:15:34 +0800

    Init

--- app/src/main/java/com/austinting/materialdesignpractice/MainActivity.java
+++ app/src/main/java/com/austinting/materialdesignpractice/MainActivity.java
@@ -3,11 +3,13 @@ package com.austinting.materialdesignpractice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Create by Austin Ting 2016/03/18
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
